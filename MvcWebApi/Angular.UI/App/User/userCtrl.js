c00a2a11bdc923393302fbbadee7955779a27ccc
JonathanSawyer <JonathanSawyer@users.noreply.github.com>
2016-08-14 13:40:48 +0900

    Request a user to be added

--- MvcWebApi/Angular.UI/App/User/userCtrl.js
+++ MvcWebApi/Angular.UI/App/User/userCtrl.js
@@ -1,4 +1,39 @@
-﻿angular.module('myApp.controllers').controller("UserCtrl", function ($scope)
+﻿angular.module('myApp.controllers').controller("UserCtrl", function ($scope, $http)
 {
     $scope.test = "Hello World";
+    $scope.users = [];
+    $scope.user =
+        {
+            Name: "",
+            Surname: ""
+        };
+
+    $scope.pageMode = {}
+    $scope.pageMode.setList = function ()
+    {
+        $scope.pageMode.list = true;
+        $scope.pageMode.edit = false;
+    }
+
+    $scope.pageMode.setEdit = function ()
+    {
+        $scope.pageMode.list = false;
+        $scope.pageMode.edit = true;
+    }
+
+    $scope.pageMode.setList();
+
+    $http.get("../api/user").success(function (users)
+    {
+        $scope.users = users;
+    });
+
+    $scope.save = function()
+    {
+        //TODO: Add response
+        $http.post("../api/user", $scope.user).success(function (user)
+        {
+            
+        });
+    }
 });
