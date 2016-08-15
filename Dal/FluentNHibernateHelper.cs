ce62c377872c60ae3111e6b40abbe1fc01f17c62
JonathanSawyer <JonathanSawyer@users.noreply.github.com>
2016-08-15 14:35:15 +0900

    Export Configuration for SqlLite

--- Dal/FluentNHibernateHelper.cs
+++ Dal/FluentNHibernateHelper.cs
@@ -18,16 +18,16 @@ namespace RateIT.Example.DalMappings
         public static ISessionFactory CreateSessionFactory()
         {
             return Fluently.Configure()
-                .Database(SQLiteConfiguration.Standard.UsingFile(@"C:\Development\RateITTest\Deployment\WorkflowApplication.db"))
+                .Database(SQLiteConfiguration.Standard.UsingFile(@"C:\WorkflowApplication.db"))
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>()/*.ExportTo(@"C:\Development\RateITTest\MvcApplication1\Dal\GeneratedORMFiles\")*/)
-                //.ExposeConfiguration(BuildSchema)
+                .ExposeConfiguration(BuildSchema)
                 .BuildSessionFactory();
         }
 
         private static void BuildSchema(Configuration config)
         {
-            if (File.Exists(@"C:\Development\RateITTest\Deployment\WorkflowApplication.db"))
-                File.Delete(@"C:\Development\RateITTest\Deployment\WorkflowApplication.db");
+            if (File.Exists(@"C:\WorkflowApplication.db"))
+                File.Delete(@"C:\WorkflowApplication.db");
 
             new SchemaExport(config)
               .Create(false, true);
