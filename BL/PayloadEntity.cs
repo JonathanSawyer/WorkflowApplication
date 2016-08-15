f1e90bf25d5fdc9c989ae4bdf31e0fe8bd31d5c2
JonathanSawyer <JonathanSawyer@users.noreply.github.com>
2016-08-15 12:01:55 +0900

    Payload Entity

---
+++ BL/PayloadEntity.cs
@@ -0,0 +1,16 @@
+﻿using BL.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public abstract class PayloadEntity<Entity> : Entity<int>
+    {
+        public virtual EntityStatus Status { get; set; }
+
+        public virtual IList<EntityWorkflow<Entity>> Workflows { get; set; }
+    }
+}
